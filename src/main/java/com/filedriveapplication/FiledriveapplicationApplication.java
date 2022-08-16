16a80b813a55b5c62b79c165f581e3fcc8c8b5c6
jrl2218 <93962058+jrl2218@users.noreply.github.com>
2022-08-16 13:27:58 -0400

    Initial commit - everything except AppController is from the spring initializr

---
+++ src/main/java/com/filedriveapplication/FiledriveapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.filedriveapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiledriveapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiledriveapplicationApplication.class, args);
+	}
+
+}
